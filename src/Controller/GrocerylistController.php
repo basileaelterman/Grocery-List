09be2f9b1a7287236786b2a979cc4ee605b8f10b
basileaelterman <basile.aelterman@email.com>
2025-12-10 12:31:01 +0100

    Added a security shell, simple CRUD operations and some "isolation" features

---
+++ src/Controller/GrocerylistController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\Product;
+use App\Form\ConfirmType;
+use App\Form\ProductType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class GrocerylistController extends AbstractController
+{
+    #[Route('/grocerylist', name: 'app_grocerylist')]
+    public function list(): Response
+    {
+        $user = $this->getUser();
+        if (!$user)
+        {
+            return $this->redirectToRoute('app_login', [], 401);
+        }
+
+        $groceries = $user->getProducts();
+
+        return $this->render('grocerylist/list.html.twig', [
+            'user' => $user,
+            'groceries' => $groceries,
+        ]);
+    }
+
+    #[Route('/grocerylist/{id<\d+>}', name: 'app_grocerylist_product')]
+    public function show(Product $product): Response
+    {
+        $user = $this->getUser();
+        if (!$user)
+        {
+            return $this->redirectToRoute('app_login');
+        }
+        if ($user !== $product->getOwner())
+        {
+            throw $this->createAccessDeniedException('User does not have permission to view this');
+        }
+
+        return $this->render('grocerylist/show.html.twig', [
+            'user'    => $user,
+            'product' => $product,
+        ]);
+    }
+
+    #[Route('/grocerylist/create', name: 'app_grocerylist_create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        if (!$user)
+        {
+            return $this->redirectToRoute('app_login', [], 401);
+        }
+
+        $product = new Product();
+        $product->setOwner($user);
+
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            $this->addFlash('notice', 'Successfully added product to your grocery list!');
+
+            return $this->redirectToRoute('app_grocerylist_product', ['id' => $product->getId()]);
+        }
+
+        return $this->render('grocerylist/create.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/grocerylist/{id<\d+>}/update', name: 'app_grocerylist_update')]
+    public function update(Product $product, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $user = $this->getUser();
+        if (!$user)
+        {
+            return $this->redirectToRoute('app_login', [], 401);
+        }
+        if ($user !== $product->getOwner())
+        {
+            throw $this->createAccessDeniedException('User does not have permission to view this');
+        }
+
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            $this->addFlash('notice', 'Successfully updated product from your grocery list!');
+
+            return $this->redirectToRoute('app_grocerylist_product', ['id' => $product->getId()]);
+        }
+
+        return $this->render('grocerylist/update.html.twig', [
+            'user'    => $user,
+            'product' => $product,
+            'form'    => $form,
+        ]);
+    }
+
+    #[Route('/grocerylist/{id<\d+>}/delete', name: 'app_grocerylist_delete')]
+    public function delete(Product $product, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $user = $this->getUser();
+        if (!$user)
+        {
+            return $this->redirectToRoute('app_login', [], 401);
+        }
+        if ($user !== $product->getOwner())
+        {
+            throw $this->createAccessDeniedException('User does not have permission to view this');
+        }
+
+        $form = $this->createForm(ConfirmType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager->remove($product);
+            $entityManager->flush();
+
+            $this->addFlash('notice', 'Successfully removed product from your grocery list!');
+
+            return $this->redirectToRoute('app_grocerylist');
+        }
+
+        return $this->render('grocerylist/delete.html.twig', [
+            'user'    => $user,
+            'product' => $product,
+            'form'    => $form,
+        ]);
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+#[ORM\Table(name: '`product`')]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $amount = null;
+
+    #[ORM\Column(length: 2000)]
+    private ?string $details = null;
+
+    #[ORM\ManyToOne(inversedBy: 'products')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $owner = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getDetails(): ?string
+    {
+        return $this->details;
+    }
+
+    public function setDetails(string $details): static
+    {
+        $this->details = $details;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): static
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+}
